23bd1b2fd1a4506b870eec2a93bc836994cab848
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-07-18 08:56:21 +0100

    Initial commit

---
+++ src/main/java/ictech/u2_w1_d5_booking_management_project/U2W1D5BookingManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package ictech.u2_w1_d5_booking_management_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W1D5BookingManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W1D5BookingManagementProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ictech/u2_w1_d5_booking_management_project/U2W1D5BookingManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ictech.u2_w1_d5_booking_management_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W1D5BookingManagementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
